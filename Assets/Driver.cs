fe58d9c128e331d700b54d04f73b9ec8447dbefe
ChiantiWine <hsm11256@naver.com>
2024-11-19 15:41:10 +0900

    2024.11.19
    Unity Driver
    1. transform.Rotate

---
+++ Assets/Driver.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Doriver : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+        // transform.Rotate(Vector3.forward, Space.Self);
+    }
+
+   
+    void Update()
+    {
+        transform.Rotate(0, 0, 45);
+    }
+}
