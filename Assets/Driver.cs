7992becc4354ecf082b924b98f3d6076a21b02ee
ChiantiWine <hsm11256@naver.com>
2024-11-27 15:47:58 +0900

    collision
    1. Collider 추가(형태에 상관없이 Box, Capsule, circle Collider 추가 가능)
    2. rigidbody 추가(Gravity 0 - RPG나 점프가 있는 게임에 적용)

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -16,7 +16,7 @@ public class Doriver : MonoBehaviour
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; // 매프레임마다 새롭게 계산해야하기 때문에 Update에 씀
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // Time.deltaTime을 적용하면 느린,빠른 컴퓨터에서도 같은 속도를 가짐
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // Time.deltaTime을 적용해서 각 프레임이 실행되는 데 시간이 얼마나 소요되는지 알려줌
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0,  moveAmount, 0);
     }
