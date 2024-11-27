864ec6b3121f0b36e73672aac9ce7d97605bb67c
ChiantiWine <hsm11256@naver.com>
2024-11-27 15:20:48 +0900

    Time.delaTime
    1. -steerAmount  변경
    2. 속도 조절
    3. Time.deltaTime 추가

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -15,9 +15,9 @@ public class Doriver : MonoBehaviour
    
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed; // 매프레임마다 새롭게 계산해야하기 때문에 Update에 씀
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
-        transform.Rotate(0, 0, steerAmount);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; // 매프레임마다 새롭게 계산해야하기 때문에 Update에 씀
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // Time.deltaTime을 적용하면 느린,빠른 컴퓨터에서도 같은 속도를 가짐
+        transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0,  moveAmount, 0);
     }
 }
