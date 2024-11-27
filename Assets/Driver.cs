26b541564268340f3713aad08ff50476dd303de6
ChiantiWine <hsm11256@naver.com>
2024-11-27 14:03:03 +0900

    variable
    variable added
    steerSpeed
    moveSpeed

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Doriver : MonoBehaviour
 {
-    
+    float steerSpeed = 0.1f;
+    float moveSpeed = 0.01f;
+
     void Start()
     {
         
@@ -13,7 +15,7 @@ public class Doriver : MonoBehaviour
    
     void Update()
     {
-        // transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.1f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
