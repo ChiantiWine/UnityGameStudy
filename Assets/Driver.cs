d8d2ea3b5f3bbe6b2d2fbed2ddd479dfef1d4327
ChiantiWine <hsm11256@naver.com>
2024-11-27 15:03:56 +0900

    Input
    Inout.GetAxis

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -15,7 +15,9 @@ public class Doriver : MonoBehaviour
    
     void Update()
     {
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed; // 매프레임마다 새롭게 계산해야하기 때문에 Update에 씀
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0,  moveAmount, 0);
     }
 }
